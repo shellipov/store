83f81a68b258c4e9314652fd0598903912e7557f
Шелипов Дмитрий <dshelipov@cloudfactory.ru>
2025-10-16 16:32:06 +0300

    change app name

--- android/app/src/main/java/com/store/bundleid/MainActivity.kt
+++ android/app/src/main/java/com/store/bundleid/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.d2c
+package com.store.bundleid
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -11,7 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "D2C"
+  override fun getMainComponentName(): String = "Store"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
--- android/app/src/main/java/com/store/bundleid/MainApplication.kt
+++ android/app/src/main/java/com/store/bundleid/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.d2c
+package com.store.bundleid
 
 import android.app.Application
 import com.facebook.react.PackageList
